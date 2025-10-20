12b60bd9c2055da179533429606d4db776867410
anjaanjaa10 <anjacolic2000@gmail.com>
2025-10-20 02:15:46 +0200

    Add .gitignore

--- clang-tidy/build/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_tests_FILE /home/anja/2024_Analysis_MatfTunes/clang-tidy/build/tests/tests)
-set(__QT_DEPLOY_TARGET_tests_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MatfTunes_FILE /home/anja/2024_Analysis_MatfTunes/clang-tidy/build/MatfTunes)
-set(__QT_DEPLOY_TARGET_MatfTunes_TYPE EXECUTABLE)
--- clang-tidy/build/CMakeFiles/MatfTunes_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for MatfTunes_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
